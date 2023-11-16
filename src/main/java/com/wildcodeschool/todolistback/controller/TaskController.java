f6a86c2b9b846d3553ed3fe49794446261592877
Spolito24 <baptisterenier69@gmail.com>
2023-11-16 12:17:53 +0100

    improve app by adding null verification and try catch

--- src/main/java/com/wildcodeschool/todolistback/controller/TaskController.java
+++ src/main/java/com/wildcodeschool/todolistback/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.wildcodeschool.todolistback.repository.TaskRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,28 +26,48 @@ public class TaskController {
     }
 
     @PostMapping("/task")
-    public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        return new ResponseEntity<>(taskRepository.save(task), HttpStatus.CREATED);
+    public ResponseEntity<?> createTask(@RequestBody Task task) {
+        if (task == null) {
+            return ResponseEntity.badRequest().body("Task data must not be null");
+        }
+        try {
+            return ResponseEntity.ok(taskRepository.save(task));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating task");
+        }
     }
 
-    @PutMapping("/task/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable long id, @RequestBody Task task) {
-        Optional<Task> taskToUpdate = this.taskRepository.findById(id);
-        if (taskToUpdate.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } else {
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        if (task == null) {
+            return ResponseEntity.badRequest().body("Task data must not be null");
+        }
+
+        try {
+            Optional<Task> taskToUpdate = taskRepository.findById(id);
+            if (taskToUpdate.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task Not Found");
+            }
             Task taskUpdated = taskToUpdate.get();
             taskUpdated.setTitle(task.getTitle());
             taskUpdated.setDescription(task.getDescription());
             taskUpdated.setDone(task.isDone());
-            return new ResponseEntity<>(taskRepository.save(taskUpdated), HttpStatus.OK);
+
+            return ResponseEntity.ok(taskRepository.save(taskUpdated));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating task");
         }
     }
 
     @DeleteMapping("/task/{id}")
-    public ResponseEntity<HttpStatus> deleteTask(@PathVariable long id) {
-        this.taskRepository.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<?> deleteTask(@PathVariable long id) {
+        try {
+            taskRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Task deleted");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting task");
+        }
     }
 }
 
